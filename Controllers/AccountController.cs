895c1b20881e17dd3ed0347f50f227d17f6a93fb
dangngoc0311 <ngoc1233456789@gmail.com>
2023-08-16 20:28:49 +0700

    Task 4 : Fix image, video

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -38,12 +38,12 @@ namespace DoAN_k4.Controllers
 
                 if (checkPass != false && acc.IsAdmin == true)
                 {
-                    var identity = new ClaimsIdentity(new[] { 
+                    var identity = new ClaimsIdentity(new[] {
                         new Claim(ClaimTypes.Name, acc.Email),
-                        new Claim("UserId", acc.Id), 
+                        new Claim("UserId", acc.Id),
                         new Claim("FullName", acc.FirstName + " "+ acc.LastName),
                         new Claim("Email", acc.Email),
-                        new Claim("ImageUser", acc.UserImage),
+                     new Claim("ImageUser", acc.UserImage ?? "https://media.istockphoto.com/id/1223671392/vi/vec-to/%E1%BA%A3nh-h%E1%BB%93-s%C6%A1-m%E1%BA%B7c-%C4%91%E1%BB%8Bnh-h%C3%ACnh-%C4%91%E1%BA%A1i-di%E1%BB%87n-ch%E1%BB%97-d%C3%A0nh-s%E1%BA%B5n-cho-%E1%BA%A3nh-minh-h%E1%BB%8Da-vect%C6%A1.jpg?s=170667a&w=0&k=20&c=HA19gorpprPTQ4wQIP4ZQAn4EDr55qbbB-yBOEsLVCs="),
                     }, "DoAnKy4Security");
                     var pricipal = new ClaimsPrincipal(identity);
                     await HttpContext.SignInAsync("DoAnKy4Security", pricipal);
@@ -54,7 +54,7 @@ namespace DoAN_k4.Controllers
                     TempData["ErrorMessage"] = "Thông tin đăng nhập không hợp lệ!";
                     return View("Index");
                 }
-                
+
             }
             else
             {
